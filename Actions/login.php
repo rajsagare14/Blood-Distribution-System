c17e9adc79a301186c4277f9857002b12acc35de
rajsagare14 <58337889+rajsagare14@users.noreply.github.com>
2022-10-15 12:17:57 +0530

    Add files via upload

---
+++ Actions/login.php
@@ -0,0 +1,9 @@
+<?php
+
+echo $_POST['username'];
+echo $_POST['password'];
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+?>
---
+++ Actions/signup-bank.php
@@ -0,0 +1,41 @@
+<?php
+echo $_POST['hospCity'];
+$hospCity = $_POST['hospCity'];
+echo $_POST['bldbankName'];
+$bldbankName = $_POST['bldbankName'];
+echo $_POST['parentHospName'];
+$parentHospName = $_POST['parentHospName'];
+echo $_POST['hospShortName'];
+$hospShortName = $_POST['hospShortName'];
+echo $_POST['contactPerson'];
+$contactPerson = $_POST['contactPerson'];
+echo $_POST['hospEmail'];
+$hospEmail = $_POST['hospEmail'];
+echo $_POST['hospContact'];
+$hospContac = $_POST['hospContact'];
+echo $_POST['hospFax'];
+$hospFax = $_POST['hospFax'];
+echo $_POST['licenceNo'];
+$licenceNo= $_POST['licenceNo'];
+echo $_POST['licenceFromDate'];
+$licenceFromDate= $_POST['licenceFromDate'];
+echo $_POST['licenceToDate'];
+$licenceToDate= $_POST['licenceToDate'];
+echo $_POST['helplineNo'];
+$helplineNo= $_POST['helplineNo'];
+echo $_POST['hospAdd1'];
+$hospAdd1= $_POST['hospAdd1'];
+echo $_POST['hospAdd2'];
+$hospAdd2= $_POST['hospAdd2'];
+echo $_POST['pincode'];
+$pincode= $_POST['pincode'];
+echo $_POST['hospLatitude'];
+$hospLatitude= $_POST['hospLatitude'];
+echo $_POST['hospLongitude'];
+$hospLongitude= $_POST['hospLongitude'];
+echo $_POST['hospWebsite'];
+$hospWebsite= $_POST['hospWebsite'];
+echo $_POST['noOfBed'];
+$noOfBed= $_POST['noOfBed'];
+
+?>
