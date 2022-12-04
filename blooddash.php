75cd1ef41fc9d7dc2316c611b85c81b148878a3e
DHANRAJ BHARAT PANCHAL <48233707+Dhanraj01@users.noreply.github.com>
2022-12-04 18:05:54 +0530

    Add files via upload

---
+++ blooddash.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../Assets/css/dash.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Bank</title>
+</head>
+<body>
+    <h1 class = "companyName">Blood Bank Name</h1>
+    <div class="search">
+            <form action="#">
+                <input type="text"
+                    placeholder=" Search"
+                    name="search">
+                <button>
+                    <i class="fa fa-search"
+                        style="font-size: 18px;">
+                    </i>
+                </button>
+                
+            </form>
+        </div>
+    </div>
+    <h2>Available Blood Stocks</h2>
+<div class="a">
+<div id="a+ve">
+        <h3 class="bloodGroup">A +ve</h3>
+        <div  class = "stocks">
+            <h4 class="wholeBlood">999</h4>
+            <h4 class="plasma">999</h4>
+            <h4 class="platelets">999</h4>
+        </div>
+    </div>
+<div  id="a-ve">
+        <h3 class="bloodGroup">A -ve</h3>
+        <div class = "stocks">
+            <h4 class="wholeBlood">999</h4>
+            <h4 class="plasma">999</h4>
+            <h4 class="platelets">999</h4>
+        </div>
+    </div>
+</div>
+    <br>
+<div class="b">
+<div  id="B+ve">
+        <h3 class="bloodGroup">B +ve</h3>
+        <div class = "stocks">
+            <h4 class="wholeBlood">999</h4>
+            <h4 class="plasma">999</h4>
+            <h4 class="platelets">999</h4>
+        </div>
+    </div>
+<div  id="B-ve">
+        <h3 class="bloodGroup">B -ve</h3>
+        <div class = "stocks">
+            <h4 class="wholeBlood">999</h4>
+            <h4 class="plasma">999</h4>
+            <h4 class="platelets">999</h4>
+        </div>
+    </div>
+</div>
+    <br>
+    <div class="o">
+    <div  id="o+ve">
+        <h3 class="bloodGroup">O +ve</h3>
+        <div class = "stocks">
+            <h4 class="wholeBlood">999</h4>
+            <h4 class="plasma">999</h4>
+            <h4 class="platelets">999</h4>
+        </div>
+    </div>
+    <div  id="o-ve">
+        <h3 class="bloodGroup">O -ve</h3>
+        <div class = "stocks">
+            <h4 class="wholeBlood">999</h4>
+            <h4 class="plasma">999</h4>
+            <h4 class="platelets">999</h4>
+        </div>
+    </div>
+    </div>
+    <br>
+    <div class="ab">
+    <div  id="ab+ve">
+        <h3 class="bloodGroup">AB +ve</h3>
+        <div class = "stocks">
+            <h4 class="wholeBlood">999</h4>
+            <h4 class="plasma">999</h4>
+            <h4 class="platelets">999</h4>
+        </div>
+    </div>
+    <div  id="ab-ve">
+        <h3 class="bloodGroup">AB -ve</h3>
+        <div class = "stocks">
+            <h4 class="wholeBlood">999</h4>
+            <h4 class="plasma">999</h4>
+            <h4 class="platelets">999</h4>
+        </div>
+    </div>
+    </div>
+   
+    <a href="Form.php">
+    <button formaction ="Form.php">
+                     Order Blood
+                </button>
+    </a>
+<div class="warning">Warning</div>
+</body>
+</html>
